b952cb5d0ffd09c332e076aa87189afc349f6d4f
Sergey Kavanosyan <kavanosyan@mail.ru>
2023-12-23 14:12:58 +0300

    deploy v1.1.1

--- core/logics/post_delete.py
+++ core/logics/post_delete.py
@@ -3,14 +3,14 @@ from aiogram import Bot
 from sqlalchemy.orm import sessionmaker
 
 from config import CHANEL_ID
-from core.db import Messages, set_delete_job_id
+from core.db import Messages, set_delete_job_id, get_db_message
 from core.logics import post
 from core.scheduler import add_one_job
 
 
 async def delete(db_message: Messages, bot: Bot) -> None:
     """This is post delete function"""
-
+    print('db_message.telegram_msg_id', db_message.telegram_msg_id, type(db_message.telegram_msg_id))
     # for all messages types, excepts media_group
     if isinstance(db_message.telegram_msg_id, int):
         await bot.delete_message(chat_id=CHANEL_ID, message_id=db_message.telegram_msg_id)
@@ -24,7 +24,11 @@ async def delete(db_message: Messages, bot: Bot) -> None:
 async def post_then_delete(db_message: Messages, bot: Bot, session_maker: sessionmaker) -> None:
     """This is post delete function"""
     await post(db_message, bot, session_maker)
+
+    # get refresh db_message with telegram_msg_id after
+    db_message = await get_db_message(db_message.id, session_maker)
     print('del message_id', db_message.telegram_msg_id)
+
     delete_job_id = await add_one_job(func=delete,
                                       dtime=db_message.delete_date,
                                       kwargs={'db_message': db_message, 'bot': bot}
